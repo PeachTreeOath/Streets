8ed2c116a140c4456c77ce079dfdd0e0f52e84df
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-14 00:27:53 -0600

    Basic health

--- Assets/Resources/Scripts/Game/Bullet.cs
+++ Assets/Resources/Scripts/Game/Bullet.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
+
 	private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("hit");
-        Destroy(gameObject);
-    }
+        Health health = col.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+            }
 }
