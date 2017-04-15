c9c2e86adc0b6e8d01647b7c76346aa024a9120f
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-15 03:27:23 -0600

    Networked hp

--- Assets/Resources/Scripts/Game/Health.cs
+++ Assets/Resources/Scripts/Game/Health.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
+
     public RectTransform healthBar;
     public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+        {
+            return;
+        }
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead!");
+            currentHealth = maxHealth;
+            RpcRespawn();
         }
 
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if(isLocalPlayer)
+        {
+            transform.position = Vector2.zero;
+        }
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
         healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
     }
 }
