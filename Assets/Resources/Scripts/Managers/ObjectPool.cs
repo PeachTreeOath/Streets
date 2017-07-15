0306ae8bf866f6af86992ae9f1d51c0cee9855a0
PeachTreeOath <peachtreeoath@gmail.com>
2017-07-15 02:27:23 -0600

    2017 upgrade

--- Assets/Resources/Scripts/Managers/ObjectPool.cs
+++ Assets/Resources/Scripts/Managers/ObjectPool.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour {
+public class ObjectPool : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
