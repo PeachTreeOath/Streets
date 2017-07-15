d6f18d80d4fac8a9a0fde67f9090e77609dab366
PeachTreeOath <peachtreeoath@gmail.com>
2017-07-15 02:57:37 -0600

    Bullet object pool

--- Assets/Resources/Scripts/Managers/ObjectPool.cs
+++ Assets/Resources/Scripts/Managers/ObjectPool.cs
@@ -1,16 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int bulletBudget = 100;
+    private int bulletIndex;
+    private List<GameObject> bullets = new List<GameObject>();
+
+    private GameObject objectPoolParent;
+    private GameObject bulletParent;
+
+    void Init()
+    {
+        Clear();
+
+        objectPoolParent = new GameObject();
+        bulletParent = new GameObject();
+
+        bulletParent.transform.SetParent(objectPoolParent.transform);
+
+        CreateBudget(ResourceLoader.instance.bulletFab, bulletBudget, bulletParent, bullets);
+    }
+
+    private void CreateBudget(GameObject prefab, int budget, GameObject parent, List<GameObject> list)
+    {
+        for(int i = 0; i < budget; i++)
+        {
+            GameObject go = Instantiate(prefab);
+            go.SetActive(false);
+            go.transform.SetParent(parent.transform);
+        }
+    }
+
+    public GameObject RequestObject(OP_Type type)
+    {
+        GameObject returnObj = null;
+
+        switch(type)
+        {
+            case OP_Type.bullet:
+                if (bulletIndex > bullets.Count - 1)
+                    bulletIndex = 0;
+                returnObj = bullets[bulletIndex];
+                bulletIndex++;
+                break;
+        }
+
+        return returnObj;
+    }
+
+    private void Clear()
+    {
+        if(objectPoolParent)
+            Destroy(objectPoolParent);
+        if (bulletParent)
+            Destroy(bulletParent);
+
+        bullets.Clear();
+    }
+
+    public enum OP_Type
+    {
+        bullet
+    }
 }
